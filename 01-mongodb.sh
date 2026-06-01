1db22a79315611e80f1c51e21da3b90658fdcacb
Jagan0816 <jagandevops9@gmail.com>
2026-06-01 15:05:00 +0530

    fix

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -22,7 +22,7 @@ VALIDATE(){
         echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$TIMESTAMP [INFO] $2 .... $G SUCCESS $N | tee -a $LOG_FILE
+        echo -e "$TIMESTAMP [INFO] $2 .... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 cp mongo.repo /etc/yum.repos.d/mongo.repo
